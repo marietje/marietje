e253ea8b5f13fc35fe562c48dc354658a050e6b6
Bas Westerbaan <bas@westerbaan.name>
2016-01-05 12:15:14 +0100

    salt: let pijsmarietje find out host and port itself

--- salt/states/player/pijsmarietje.config.js
+++ salt/states/player/pijsmarietje.config.js
@@ -1,11 +1,5 @@
 var pijsmarietje_config = {
     server: {
-{% if grains.get('vagrant') %}
-        host: window.location.host,
-{% else %}
-        host: '{{ grains['fqdn'] }}',
-{% endif %}
-        port: 80,
         path: '/api'
     }
 };
